6c3c9fd5db61019c8cdaf4bfaf9adefc816f9783
LudgerC <ludgerfilypecardoso@gmail.com>
2024-10-21 10:12:42 +0200

    Update M1.mjs

--- M1.mjs
+++ M1.mjs
@@ -3,3 +3,19 @@ import { stdin as input, stdout as output } from 'node:process';
 const userInput = readline.createInterface({ input, output });
 
 
+let prijzen = [1000, 100, 50, 10];
+let resultaat = 0;
+
+geefKorting(prijzen);
+
+function geefKorting(prijzen)
+{
+
+    for(let i = 0; i < prijzen.length; i++)
+    {
+        let som = (prijzen[i] / 100) * 15;
+        resultaat = prijzen[i] - som;
+        console.log(resultaat);
+    }
+
+}
