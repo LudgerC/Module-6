2951e26e547a5b4dc7fbbbc3c4978375c03ae4d1
LudgerC <ludgerfilypecardoso@gmail.com>
2024-10-24 11:18:06 +0200

    Update H2.mjs

--- H2.mjs
+++ H2.mjs
@@ -3,6 +3,10 @@ import { stdin as input, stdout as output } from 'node:process';
 const userInput = readline.createInterface({ input, output });
 
 
+let x = parseFloat(await userInput.question("Geef x in: "));
+let y = parseFloat(await userInput.question("Geef y in: "));
+
+
 let veld = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
@@ -14,4 +18,13 @@ let veld = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   ];
+
+
+shot(x, y);
+
+function shot(x, y){
+veld[x][y] = "*";
+console.log("U heeft geschoten op x positie " + x + " en op y " + y);
 console.log(veld);
+
+}
